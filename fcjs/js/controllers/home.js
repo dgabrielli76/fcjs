31767ebca2440e0daf1b11413de1f569b23bbd63
Yoann Perro <Yoann@perro-2.pusq.etud.u-picardie.fr>
2016-10-18 14:19:23 +0200

    Added order page and order creation

--- fcjs/js/controllers/home.js
+++ fcjs/js/controllers/home.js
@@ -2,9 +2,17 @@ angular.module('FC-JS').controller('HomeCtrl', function($scope, PizzAPI) {
   /* Get all pizzas */
   PizzAPI.getAllPizzas();
   $scope.pizzas = PizzAPI.listPizzas;
+  $scope.successOrder = false;
 
+  /* Format price */
   $scope.formatPrice = function(price) {
     price = price / 100;
     return price.toFixed(2);
   };
+
+  /* Create order */
+  $scope.createOrder = function(id) {
+    PizzAPI.createOrder(id);
+    $scope.successOrder = true;
+  }
 });
--- fcjs/js/controllers/orders.js
+++ fcjs/js/controllers/orders.js
@@ -1,3 +1,14 @@
 angular.module('FC-JS').controller('OrdersCtrl', function($scope, PizzAPI) {
-  
+  /* Get all orders */
+  PizzAPI.getAllOrders();
+  $scope.orders = PizzAPI.listOrders;
+
+  $scope.formatPrice = function(price) {
+    price = price / 100;
+    return price.toFixed(2);
+  };
+
+  $scope.formatDate =  function(date) {
+     return moment(date).format('DD-MM-YYYY HH:mm:ss');
+  };
 });
