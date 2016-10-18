c3b712e84ead67fbb810ccb60e5f7b59bbd3947c
dgabrielli76 <dgabrielli76@gmail.com>
2016-10-18 12:22:12 +0200

    Create services for list all orders and create orders
    This commit add services to create some orders and list all the orders.

--- fcjs/js/services/pizzapi.js
+++ fcjs/js/services/pizzapi.js
@@ -1,6 +1,7 @@
 angular.module('FC-JS').factory('PizzAPI', function($http) {
   var service = {};
   service.listPizzas = [];
+  service.listOrders = [];
 
   service.getAllPizzas = function() {
     // Get all pizzas
@@ -16,6 +17,42 @@ angular.module('FC-JS').factory('PizzAPI', function($http) {
       }, function errorCallback(response) {
         console.log(response);
         service.listPizzas = [];
+      });
+  };
+
+  service.getAllOrders = function() {
+    // Get all orders
+    $http({
+      method: 'GET',
+      url: 'https://pizzapi.herokuapp.com/orders',
+      headers: {'Authorization': 'fc->js'}
+    }).then(function successCallback(response) {
+        if(service.listOrders.length > 0) service.listOrders = [];
+        response.data.forEach(function(order) {
+          service.listOrders.push(order);
+        });
+      }, function errorCallback(response) {
+        console.log(response);
+        service.listOrders = [];
+      });
+  };
+
+  var config = {
+    headers: {
+      'Authorization': 'fc->js',
+    }
+  };
+
+  service.createOrder = function(id) {
+    // Create an order
+    $http.post(
+      'https://pizzapi.herokuapp.com/orders',
+      {id: id},
+      config
+    ).then(function successCallback(response) {
+        console.log(response);
+      }, function errorCallback(response) {
+        console.log(response);
       });
   };
 
